ae9aeb760608a727efb00a2fc86e8bc45649ae35
Fatima Avila <fatimaavila@ufm.edu>
2021-02-27 01:14:57 +0000

    ya funciona compu y ships de player

--- Main.java
+++ Main.java
@@ -6,7 +6,8 @@ class Main {
     public static int numFilas = 10;
     public static int numCols = 10;
     public static int playerShips = 3;
-    public static int compShips;
+    public static int compShips = 3;
+
     public static String[][] tablero = new String[numFilas][numCols];
     public static int[][] fallos = new int[numFilas][numCols];
 //------------------------------- métodos ---------------------------
@@ -52,58 +53,55 @@ public static void crearCampoDeBatalla(){
 public static void crearBarcosPlayer(){
     Scanner input = new Scanner(System.in);
 
-    System.out.println("\nCrear barcos del jugador:");
+    System.out.println("\nAsigne posición ");
     //Deploying five ships for player
     int x,y,barcosAsignados=0;
-
-    //ToDo: hacer while, mientras no se ingresen coordenadas validas
-    //ToDo: validar coordenadas en 1 solo metodo
-
-
-    while (barcosAsignados <= playerShips) {
+    while (barcosAsignados < playerShips) {
        
-        System.out.print("Ingresa la coordenada -X- del barco " + barcosAsignados+ ": ");
+        System.out.print(" -X- del barco " + (barcosAsignados+1) + ": ");
 
         x = input.nextInt();
         if (x>=0 && x<numFilas){
 
-                System.out.print("Ingresa la coordenada -Y- del barco " + barcosAsignados +": ");
+                System.out.print(" -Y- del barco " + (barcosAsignados+1) +": ");
                 y = input.nextInt();
-                            if(y >= 0 && y < numCols && (tablero[x][y] == " "))
+                            if(y >= 0 && y < numCols && (tablero[x][y] == "🀆"))
                                 {
-                                    tablero[x][y] =   "@";
+                                    tablero[x][y] =   "🚢";
+                                    System.out.println("---------------------- ");
                                     barcosAsignados++;
                                 }
-                                else if(y >= 0 &&  y < numCols && tablero[x][y] == "@")
-                                    System.out.println("You can't place two or more ships on the same location");
-                                else if((x < 0 || x >= numFilas) || (y < 0 || y >= numCols))
-                                    System.out.println("You can't place ships outside the " + numFilas + " by " + numCols + " tablero");
+                                else if(y >= 0 &&  y < numCols && tablero[x][y] == "🚢")
+                                    System.out.println("\u001B[31m ¡No puedes colocar dos barcos en el mismo lugar!\u001B[0m");
+                                else if((x < 0 || x > numFilas) || y < 0 || y > numCols)
+                                    System.out.println("\u001B[31m No puedes colocar barcos fuera de " + numFilas + " por " + numCols + " en el tablero\u001B[0m");
 
-        } // si la posicion x es valida 
+        } else 
+        { 
+          System.out.println("\u001B[31m No puedes colocar barcos fuera de " + numFilas + " por " + numCols + " en el tablero\u001B[0m");
+          }// si la posicion x es valida 
         
     } //fin numero de barcos a ingresar
-    //tablero[5][5] =   "X";
     printTablero("Barcos listos");
 } // barcos del jugador
 
-public static void deployComputerShips(){
-    System.out.println("\nComputer is deploying ships");
+public static void crearBarcosCompu(){
+    System.out.println("\nEl contrincante está preparando sus barcos");
     //Deploying five ships for computer
-    Main.compShips = 5;
-    for (int i = 1; i <= Main.compShips; ) {
-        //int x = (int)(Math.random() * 10);
-        //random solo puede estar 0 a 4 
+   
+    for (int i = 1; i <= compShips; ) {
         int x =  (int)(Math.random() * 10);
         int y = (int)(Math.random() * 5);
 
-        if((x >= 0 && x < numFilas) && (y >= 0 && y < numCols) && (tablero[x][y] == " "))
+        if((x >= 0 && x < numFilas) && (y >= 0 && y < numCols) && (tablero[x][y] == "🀆"))
         {
-            tablero[x][y] =   "x";
-            System.out.println(i + ". ship DEPLOYED");
+            tablero[x][y] =   "🚤";
+            System.out.println("Barco"+ i + "listo");
             i++;
         }
     }
     printTablero("Barcos listos");
+
 
 
 } // fin de los barcos de la compu
@@ -114,7 +112,9 @@ public static void deployComputerShips(){
     //---- paso 1: crear tablero
     crearCampoDeBatalla();
     printTablero("Bienvenidos a la BATALLA NAVAL");
+    //------ deploy barcos
     crearBarcosPlayer();
+    crearBarcosCompu();
    
     
     
